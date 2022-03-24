c2f746eee247854455ed1837e9c3f62ba670e32e
Ubuntu <ubuntu@ip-172-31-45-190.ec2.internal>
2022-03-24 23:03:19 +0000

    Pushing back to github with the new code

---
+++ scopeOfVariables.cpp
@@ -0,0 +1,37 @@
+// Copyright (c) 2022 Coffi All rights reserved.
+// Created by: Marc Coffi
+// Date: March, 2022
+// global vs local variables
+
+#include <iostream>
+
+// global variable
+int variableX = 25;
+
+void localVariable() {
+    // this shows what happens with local variables
+
+    int variableX = 10;
+    int variableY = 30;
+    int variableZ = variableX + variableY;
+    std::cout << "Local variableX, variableY, variableZ: " << variableX
+              << " + " << variableY << " = " << variableZ << std::endl;
+}
+
+
+void globalVariable() {
+    // this shows what happens with global variables
+
+    variableX = variableX + 1;
+    int variableY = 30;
+    int variableZ = variableX + variableY;
+    std::cout << "Local variableX, variableY, variableZ: " << variableX
+              << " + " << variableY << " = " << variableZ << std::endl;
+}
+
+int main() {
+    // this function calls local and global
+
+    localVariable();
+    globalVariable();
+}
